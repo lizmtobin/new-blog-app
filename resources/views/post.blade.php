7eaf58a0dd2588fc6f2e79119fb80496221aaf4b
liz tobin <lizmtobin@gmail.com>
2022-11-18 15:28:06 +0000

    layout template

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,31 +1,14 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
-    <article>
-        <h1>
-            <a href="/posts/<?= $post->slug ?>">
-                {{$post->title }}
-            </a>
-        </h1>
-        <div>
-            {!! $post->body !!}
-            <!-- this is the way to escape html from body() function -->
-        </div>
-    </article>
-</body>
-
-</html>
+@extends('layout')
+@section('content')
+<article>
+    <h1>
+        <a href="/posts/<?= $post->slug ?>">
+            {{$post->title }}
+        </a>
+    </h1>
+    <div>
+        {!! $post->body !!}
+        <!-- this is the way to escape html from body() function -->
+    </div>
+</article>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-
-<body>
-    @foreach ($posts as $post)
-    <article class="{{$loop->even ? 'foof' : 'bar'}}">
-        <h1>
-            <a href="/posts/{{$post->slug}}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <div>
-            {{$post->excerpt }}
-        </div>
-    </article>
-
+@extends('layout')
+@section('content')
+@foreach ($posts as $post)
+<article class=" {{$loop->even ? 'foof' : 'bar'}}">
+    <h1>
+        <a href="/posts/{{$post->slug}}">
+            {{ $post->title }}
+        </a>
+    </h1>
     <div>
-        <?= $post->excerpt ?>
+        {{$post->excerpt }}
     </div>
-    @endforeach
-</body>
-
-</html>
+</article>
+@endforeach
+@endsection
