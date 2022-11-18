43ab8a07be34f834f6af8f93b8805a84ef603c6e
liz tobin <lizmtobin@gmail.com>
2022-11-18 15:28:23 +0000

    routing failure plan

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
 Route::get('/posts/{post}', function ($slug) {
 
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::findOrFail($slug)
     ]);
 });
 //can use sql like syntax to get the post with certain syntax
